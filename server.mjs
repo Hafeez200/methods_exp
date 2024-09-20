9b3ba93489b63baf933b17f7d11622a883bc8cd9
Hafeez200 <saibhaji24@email.com>
2024-09-20 22:04:20 +0500

    handle-bodyparser

--- server.mjs
+++ server.mjs
@@ -2,3 +2,5 @@ import express from 'express';
 import bodyParser from 'body-parser'; // Assuming you still need body-parser
 const app = express();
 const PORT = 2000;
+
+const handleBodyParsing = bodyParser.json();
